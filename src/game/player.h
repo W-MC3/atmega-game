f3f30ebe450d67fd67c75a2aa37a66d8e0eacf7b
Flawake <60014996+Flawake@users.noreply.github.com>
2025-12-11 14:59:07 +0100

    Basic player movement

---
+++ src/game/player.h
@@ -0,0 +1,17 @@
+/****************************************************************************************
+* File:         player.h
+* Author:       Michiel Dirks
+* Created on:   26-11-2025
+* Company:      Windesheim
+* Website:      https://www.windesheim.nl/opleidingen/voltijd/bachelor/ict-zwolle
+****************************************************************************************/
+
+
+#ifndef ATMEGA_GAME_PLAYER_H
+#define ATMEGA_GAME_PLAYER_H
+
+void init_player();
+
+void update_player();
+
+#endif //ATMEGA_GAME_PLAYER_H
