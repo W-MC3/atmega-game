40462ef2bf82fb9ebe661f1d7ef3d7a1c51aa770
mikaib <mikaib@duck.com>
2026-01-05 09:01:33 +0100

    fix corrupted packets

--- src/net/proto.c
+++ src/net/proto.c
@@ -15,6 +15,7 @@ static uint8_t recv_index;
 static bool packet_ready;
 static bool packet_recv_start;
 static proto_packet_t current_packet;
+static uint8_t packet_id = 0;
 
 void proto_init() {
     recv_index = 0;
@@ -63,18 +64,22 @@ proto_packet_t proto_get_packet() {
 }
 
 void proto_emit(uint8_t op, uint8_t data[PROTO_PACKET_MAX_DATA_SIZE]) {
-    static uint8_t packet_id = 0;
-
     uint8_t crc = op ^ packet_id;
     for (uint8_t i = 0; i < PROTO_PACKET_MAX_DATA_SIZE; i++) {
         crc ^= data[i];
     }
 
-    uint8_t buf[PROTO_PACKET_SIZE + 1] = { 0xFF, op, packet_id++, crc }; // 0xFF = start bit
+    uint8_t buf[PROTO_PACKET_SIZE + 1] = { 0 }; // 0xFF = start bit
+    buf[0] = 0xFF;
+    buf[1] = op;
+    buf[2] = packet_id++;
+    buf[3] = crc;
+
     memcpy(&buf[4], data, PROTO_PACKET_MAX_DATA_SIZE); // copies in the remaining data
 
     while (!txAvailable()) {} // perhaps we should use a callback or interrupt?
     sendUartData(buf, PROTO_PACKET_SIZE + 1);
+    while (!txAvailable()) {} // otherwise buf[4] gets evicted from the stack... aaahh
 }
 
 uint32_t proto_get_uint32(proto_packet_t* packet, uint8_t idx) {
